860a0a76e7069ed5e7696f6600dfcbd5a8dbadcf
Roberra <roberra@nova>
2012-07-01 18:33:25 -0400

    Directory changes

---
+++ riff/riff.ino
@@ -0,0 +1,26 @@
+
+ 
+int g = 8; 
+int y = 10;
+int r = 12;
+
+void setup(){
+ pinMode(g, OUTPUT);
+ pinMode(y, OUTPUT);
+ pinMode(r, OUTPUT); 
+}
+
+void loop(){
+  digitalWrite(g, HIGH);
+  delay(300);
+  digitalWrite(y, HIGH);
+  delay(300);
+  digitalWrite(r, HIGH);
+  delay(300);
+  digitalWrite(r, LOW);
+  delay(300);
+  digitalWrite(y, LOW);
+  delay(300);
+  digitalWrite(g, LOW);
+  delay(125);
+}
