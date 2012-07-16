d0e3f1ba9da0b38c63ce82a8f42598a4040fbbe8
Roberra <roberra@nova>
2012-07-16 00:29:24 -0400

    New pattern added, no longer recurive calls to logic

--- lucidity/lucidity.ino
+++ lucidity/lucidity.ino
@@ -8,19 +8,45 @@ void setup(){
  pinMode(g, OUTPUT);
  pinMode(y, OUTPUT);
  pinMode(r, OUTPUT); 
+
+  delay(1000); //Delay 10 seconds (milliseconds)
+  riff();
+  delay(1000);
+  riff();
+  delay(1000);
+  riff();
+  delay(500);
+  flash();
+  delay(1000);
+  flash();  
+  delay(1000);
+  flash();
 }
 
-void loop(){
+void riff(){
   digitalWrite(g, HIGH);
   delay(300);
+  digitalWrite(g, LOW);  
+  delay(300);
   digitalWrite(y, HIGH);
+  delay(300);
+  digitalWrite(y, LOW);
   delay(300);
   digitalWrite(r, HIGH);
   delay(300);
   digitalWrite(r, LOW);
+}
+
+void flash(){
+  digitalWrite(g, HIGH);
+  digitalWrite(y, HIGH);
+  digitalWrite(r, HIGH);
   delay(300);
+  digitalWrite(r, LOW);
   digitalWrite(y, LOW);
-  delay(300);
-  digitalWrite(g, LOW);
-  delay(125);
+  digitalWrite(g, LOW);  
+}
+
+void loop(){
+ // setup();
 }
